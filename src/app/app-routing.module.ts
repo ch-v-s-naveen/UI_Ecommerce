21ecae85beea207735bb7ba3701f42b601a537ec
ch-v-s-naveen <vsatya951@gmail.com>
2024-02-06 23:00:00 +0530

    Changes related to Footer appComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "l",
+    redirectTo : "login",
+    pathMatch : "full"
+   
+  },
+  
+    {
+      path: "login",
+      component: LoginComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
